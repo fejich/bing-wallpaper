c9820510f284c7e58f9d5dfed795e8ede6e45c27
fejich <fejich@qq.com>
2021-09-08 00:24:50 +0800

    文件名添加版权信息

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import urllib.request as req
 import json
+import re
+
 
 url = 'http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1614319565639&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160'
 
@@ -12,9 +14,11 @@ api = json.loads(res)
 api = api['images']
 downloadURL = "https://bing.com" + api[0]['url']
 dateTime = api[0]['startdate']
+copyright = api[0]['copyright'] # 获取版权信息
+rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
+new_copyright = re.sub(rstr, "_", copyright)  # 替换为下划线
 
-req.urlretrieve(downloadURL, './wallpaper/{}.jpg'.format(dateTime))
+req.urlretrieve(downloadURL, "./wallpaper/{0}-{1}.jpg".format(dateTime, new_copyright))
 
 # except:
 #     print("Error.")
-    
