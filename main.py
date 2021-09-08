d217c70fecbf8d94663ef7fadef791eac4ee0598
fejich <fejich@qq.com>
2021-09-08 10:37:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ api = json.loads(res)
 
 api = api['images']
 downloadURL = "https://bing.com" + api[0]['url']
-dateTime = api[0]['enddate']
+dateTime = api[0]['startdate']
 copyright = api[0]['copyright'] # 获取版权信息
 rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
 new_copyright = re.sub(rstr, "_", copyright)  # 替换为下划线
