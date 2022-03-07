8b69bc6b31641297fc43771796bb35aeeb1cd51b
fejich <fejich@qq.com>
2022-03-07 18:26:13 +0800

    下载完整无压缩壁纸
    不再指定下载 4k分辨率图片

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 import re
 
 
-url = 'http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1614319565639&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160'
+url = 'https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
 
 # try:
 res = req.urlopen(url)
@@ -12,11 +12,13 @@ res = res.read().decode('utf-8')
 api = json.loads(res)
 
 api = api['images']
-downloadURL = "https://bing.com" + api[0]['url']
+downloadURL = "https://bing.com" + api[0]['urlbase'] + "_UHD.jpg"
 dateTime = api[0]['startdate']
 copyright = api[0]['copyright'] # 获取版权信息
-rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
-new_copyright = re.sub(rstr, "_", copyright)  # 替换为下划线
+rstr = r'[\/\\\:\*\?\"\<\>\|]'  # '/ \ : * ? " < > |'
+new_copyright = re.sub(rstr, '_', copyright)  # 替换为下划线
+
+#print(downloadURL)
 
 req.urlretrieve(downloadURL, "./Wallpaper/{0}{1}{2}.jpg".format(dateTime, '@', new_copyright))
 req.urlretrieve(url, "./Wallpaper/{}.json".format(dateTime))
